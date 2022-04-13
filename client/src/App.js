933d3996a1e89ed8283015d668f4e725cd01c881
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-04-14 00:43:16 +0800

    Changed self-made components into grommet components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,56 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
 // import Feedback from "./pages/Feedback";
 import Request from "./pages/Request";
-import { Grommet } from "grommet";
+import { Grommet, Footer, Main, Header, Box, Anchor } from "grommet";
+import { Tools } from "grommet-icons";
+import styled from "styled-components";
+
+const LogoSection = styled.div`
+  max-width: 25%;
+  width: 20%;
+  border: 1px solid black;
+  & > img {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 function App() {
   return (
     <Grommet>
       <div className="App">
-        <NavBar />
-        <Request />
-        <Footer />
+        <Header
+          background="light-1"
+          justify="between"
+          height="3.5em"
+          pad="small-top"
+        >
+          <Box pad="xxsmall">
+            <Anchor icon={<Tools color="accent-4" size="large" />} />
+          </Box>
+          <Box justify="evenly" direction="row" width="medium">
+            <Box>
+              <Anchor href="##">Discover</Anchor>
+            </Box>
+            <Box>
+              <Anchor href="##">List</Anchor>
+            </Box>
+            <Box>
+              <Anchor href="##">Register</Anchor>
+            </Box>
+          </Box>
+        </Header>
+        <Main>
+          <Request />
+        </Main>
+        <Footer
+          height="xsmall"
+          border={{ top: "1px solid black" }}
+          justify="center"
+        >
+          <b>Copyright © HandyWork 2022. All Rights Reserved.</b>
+        </Footer>
       </div>
     </Grommet>
   );
