7934eebad8b817c90a3757453192ea1aae538970
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-04-15 20:14:13 +0800

    updated request to have a property of status

--- client/src/API/order.js
+++ client/src/API/order.js
@@ -6,11 +6,34 @@ export const postRequestByEmployer = (requestForm) => {
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ contactNo, minRate, description }),
+    body: JSON.stringify({
+      contactNo,
+      minRate,
+      description,
+      status: "pending",
+    }),
   };
 
   try {
     fetch(`http://localhost:${PORT}/requests`, requestOptions)
+      .then((res) => res.json())
+      .then((request) => request.id);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const acceptRequest = (requestId, isAccepted) => {
+  const requestOptions = {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      status: isAccepted ? "accepted" : "rejected",
+    }),
+  };
+
+  try {
+    fetch(`http://localhost:${PORT}/requests/${requestId}`, requestOptions)
       .then((res) => res.json())
       .then((request) => request.id);
   } catch (err) {
