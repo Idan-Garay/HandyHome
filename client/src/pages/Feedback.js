13c17cb2f8a74019fec82fa833e86ff4b69c8d74
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-04-15 18:33:27 +0800

    Feedback page only renders functional components

--- client/src/pages/Feedback.js
+++ client/src/pages/Feedback.js
@@ -1,95 +1,13 @@
-import React, { useState } from "react";
-import { postFeedback } from "../API/feedback";
-import OrderDetails from "../components/Order/OrderDetails";
-import Rating from "../components/Order/Rating";
-import { Box, Page, PageContent, Form, Text, TextArea, Button } from "grommet";
+import React from "react";
+import { Page, PageContent } from "grommet";
+import HandymanFeedback from "../components/Order/HandymanFeedback";
+import EmployerFeedback from "../components/Order/EmployerFeedback";
 
-const Feedback = () => {
-  const [ratings, setRatings] = useState({
-    communication: 0,
-    service: 0,
-    recommend: 0,
-  });
-
-  const [description, setDescription] = useState("");
-
-  const onTextAreaChange = (e) => {
-    setDescription(e.target.value);
-  };
-
-  const onRate = (e) => {
-    const field = e.target.name;
-    const newRatings = { ...ratings };
-    newRatings[field] = e.target.value;
-    setRatings(newRatings);
-  };
-
-  const onFormSubmit = (e) => {
-    e.preventDefault();
-    postFeedback({ type: "EmployerToHandyman", ratings, description });
-    setDescription("");
-    setRatings({ communication: 0, service: 0, recommend: 0 });
-  };
-
+const Feedback = ({ userType = 1 }) => {
   return (
     <Page kind="wide" pad="0 2em">
       <PageContent background="light" border={true} round="small">
-        <Form onSubmit={onFormSubmit}>
-          <Box width="xlarge" direction="row" pad="medium" justify="evenly">
-            <Box>
-              <Rating
-                ratingLabel="Communication with Handyman"
-                onClick={onRate}
-                ratingName="communication"
-                rate={ratings.communication}
-              />
-              <Rating
-                ratingLabel="Service as described"
-                onClick={onRate}
-                ratingName="service"
-                rate={ratings.service}
-              />
-              <Rating
-                ratingLabel="Buy again or recommend"
-                onClick={onRate}
-                ratingName="recommend"
-                rate={ratings.recommend}
-              />
-            </Box>
-            <Box direction="column">
-              <OrderDetails />
-              <Box
-                direction="column"
-                gap="xxsmall"
-                wrap
-                margin={{ top: "1em" }}
-              >
-                <Text textAlign="start" weight={500}>
-                  Description
-                </Text>
-                <Box background="#f8f8f8">
-                  <TextArea
-                    value={description}
-                    rows={10}
-                    width="xlarge"
-                    cols={50}
-                    resize={false}
-                    onChange={onTextAreaChange}
-                  />
-                </Box>
-              </Box>
-
-              <Box
-                tag="footer"
-                margin={{ top: "medium" }}
-                direction="row"
-                justify="end"
-              >
-                <Button type="submit" primary label="Send" c />
-              </Box>
-            </Box>
-          </Box>
-        </Form>
+        {userType === 1 ? <HandymanFeedback /> : <EmployerFeedback />}
       </PageContent>
     </Page>
   );
