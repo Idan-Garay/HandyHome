2377af57bb0f3492b5f6702b37773492a1a3634f
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-04-16 17:53:47 +0800

    Created Discovery Page

---
+++ client/src/pages/Discovery.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileCard from "../components/Discovery/ProfileCard";
+
+const Discovery = () => {
+  return (
+    <div>
+      <ProfileCard />
+    </div>
+  );
+};
+
+export default Discovery;
