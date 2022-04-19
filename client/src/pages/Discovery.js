6738b77e37b4047e687ec8cacdd028fb1443dc8b
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-04-19 19:55:32 +0800

    Added a discovery page

--- client/src/pages/Discovery.js
+++ client/src/pages/Discovery.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProfileCard from "../components/Discovery/ProfileCard";
+import { Box } from "grommet";
+
+import { getProfiles } from "../API/profiles";
+
+let profilesCache = [];
 
 const Discovery = () => {
+  const [profiles, setProfiles] = useState([]);
+
+  useEffect(() => {
+    if (profiles.cache !== 0) {
+      setProfiles(profilesCache);
+    }
+    getProfiles().then((json) => {
+      if (profilesCache != json) setProfiles(json);
+    });
+  }, [profilesCache]);
+
   return (
-    <div>
-      <ProfileCard />
-    </div>
+    <Box direction="row-responsive" wrap gap="small" margin="xlarge">
+      {profiles.length
+        ? profiles.map((profile, idx) => (
+            <ProfileCard profileData={profile} key={idx} />
+          ))
+        : null}
+    </Box>
   );
 };
 
