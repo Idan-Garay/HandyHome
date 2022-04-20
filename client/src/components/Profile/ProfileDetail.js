68503b0920a542142a7665bd8f615c1362eae697
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-04-21 07:03:13 +0800

    Profile Detail part component

---
+++ client/src/components/Profile/ProfileDetail.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Box, Text } from "grommet";
+import styled from "styled-components";
+
+const StyledH2 = styled.h2`
+  text-align: left;
+`;
+
+const ProfileDetail = () => {
+  return (
+    <Box
+      justify="start"
+      pad="small"
+      align="start"
+      gap="small"
+      fill="horizontal"
+      wrap
+    >
+      <StyledH2 className="textAlignLeft">Hildevon von Knechtbert</StyledH2>
+      <Text as="h4" color="gray" textAlign="start">
+        masonry, construction, gardener
+      </Text>
+      <Box fill="horizontal" wrap>
+        <Text as="p" textAlign="start">
+          Brokers and traders and money moves field bet customer value chain
+          emerging markets piker embracing ingenuity. Mortgage the colour of
+          money block chain transactions makin' chedda and money moves discount
+          window lending bitcoin protocol short traders. Billionaire hedge fund
+          manager earnings bear market all ab afdsfsdf
+        </Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default ProfileDetail;
