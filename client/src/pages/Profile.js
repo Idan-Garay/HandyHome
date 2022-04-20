8e15c7035232c4b5498e64de61db0d13d622359f
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-04-20 19:03:54 +0800

    Added Profile Component in the Profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Page, PageContent } from "grommet";
+import ProfileComponent from "../components/Profile/ProfileComponent";
 
 const Profile = () => {
   return (
     <Page kind="wide" pad="0 2em">
       <PageContent background="light" border={true} round="small">
-        Profile Page
+        <ProfileComponent />
       </PageContent>
     </Page>
   );
