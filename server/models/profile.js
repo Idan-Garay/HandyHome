1e800824256a1fd2e7032263ed250dcedf0311e7
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-05-14 06:47:12 +0800

    created model for profile validation

--- server/models/profile.js
+++ server/models/profile.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       this.belongsTo(models.User);
       this.hasOne(models.Address);
+      this.hasOne(models.ProfileValidation);
     }
   }
   Profile.init(
