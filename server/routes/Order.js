50cb94db635796833ef4583c4a630a75ce5abd42
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-05-13 11:42:52 +0800

    created routes for order and payment

--- server/routes/Order.js
+++ server/routes/Order.js
@@ -6,16 +6,66 @@ router.post("/request", async (req, res) => {
   try {
     const { from, to, price, description, status, contactNo } = req.body;
 
-    let profile = await db.Order.create({
-      from,
-      to,
+    let order = await db.Order.create({
+      fromUserId: from,
+      toUserId: to,
       price,
       description,
-      status,
       contactNo,
     });
 
-    res.status(200).jsonp(profile);
+    res.status(200).jsonp(order);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.patch("/request/accept", async (req, res) => {
+  try {
+    const { order } = req.body;
+
+    const dbOrder = await db.Order.findOne({ where: { id: order.id } });
+
+    if (dbOrder) {
+      const status = order.status == 1 ? "accepted" : "rejected";
+
+      dbOrder.set({ status: status });
+      await dbOrder.save();
+
+      res.status(200).json(`Request Order ${status}`);
+    } else {
+      res.status(200).json("Order doesn't exist");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.post("/request/validate_payment", async (req, res) => {
+  try {
+    const { isAccepted, orderId, employerUserId, handymanUserId, accountType } =
+      req.body;
+    if (accountType === 0) {
+      const dbValidatePayment = await db.PaymentValidation.create({
+        isAccepted,
+        OrderId: orderId,
+        UserId: employerUserId,
+      });
+      const dbValidatePayment2 = await db.PaymentValidation.create({
+        isAccepted: 0,
+        OrderId: orderId,
+        UserId: handymanUserId,
+      });
+    } else if (accountType === 1) {
+      const dbValidatePayment = await db.PaymentValidation.findOne({
+        where: { OrderId: orderId, UserId: handymanUserId },
+      });
+      dbValidatePayment.isAccepted = isAccepted;
+      dbValidatePayment.save();
+    }
+    res
+      .status(200)
+      .json(`Payment was ${isAccepted ? "confirmed" : "non-confirmed"}`);
   } catch (e) {
     console.log(e);
   }
