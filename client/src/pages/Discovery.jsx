99a2416f518f1bd4cb08813fd4a6e8a2f75774c5
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-05-22 21:24:37 +0800

    Revamped Discovery

--- client/src/pages/Discovery.jsx
+++ client/src/pages/Discovery.jsx
@@ -11,6 +11,7 @@ import Carpentry from "/assets/tools.png";
 import Gardening from "/assets/gardening.png";
 import BabySitting from "/assets/baby-stroller.png";
 import Masonry from "/assets/brickwork.png";
+import { Link } from "react-router-dom";
 
 let profilesCache = [];
 
@@ -134,65 +135,75 @@ const Discovery = () => {
 };
 
 const ListItem = ({ profile }) => {
-  const { name, services, email, contactNo, description, picture } = profile;
+  const { id, name, services, email, contactNo, description, picture } =
+    profile;
   return (
-    <Box height="small" width="full" pad="0 2em">
-      <Box
-        direction="row-responsive"
-        justify="evenly"
-        fill="horizontal"
-        border={{ side: "top", color: "#aeaeae", size: "1px" }}
-        pad=".5em 1em"
-        height="large"
-      >
-        <Box direction="row" gap="medium">
-          <Box width="8em">
-            <Avatar size="large" background="accent-3" className="b-1">
-              {picture ? (
-                <img
-                  src={"data:image/jpg;base64," + picture}
-                  alt="profilePic"
-                  width="100%"
-                  height="100%"
-                />
-              ) : (
-                <User size="large" />
-              )}
-            </Avatar>
+    <Link
+      to={`profiles/${id}`}
+      state={profile}
+      style={{ textDecoration: "none", color: "black" }}
+    >
+      <Box height="small" width="full" pad="0 2em">
+        <Box
+          direction="row-responsive"
+          justify="evenly"
+          fill="horizontal"
+          border={{ side: "top", color: "#aeaeae", size: "1px" }}
+          pad=".5em 1em"
+          height="large"
+        >
+          <Box direction="row" gap="medium">
+            <Box width="8em">
+              <Avatar size="large" background="accent-3" className="b-1">
+                {picture ? (
+                  <img
+                    src={"data:image/jpg;base64," + picture}
+                    alt="profilePic"
+                    width="100%"
+                    height="100%"
+                  />
+                ) : (
+                  <User size="large" />
+                )}
+              </Avatar>
+            </Box>
           </Box>
-        </Box>
 
-        <Box gap="medium" width="90%">
-          <Box width="medium">
-            <Heading level={3} textAlign="start">
-              {name}
-            </Heading>
-            <Heading level={5} textAlign="start">
-              {services
-                .split(",")
-                .map((service) => {
-                  return service[0].toUpperCase() + service.slice(1);
-                })
-                .join(", ")}
-            </Heading>
-          </Box>
-          <Text textAlign="start">" {description} "</Text>
-          <Box direction="row-responsive" gap="xlarge">
-            <Text>
-              Email: <strong>{email}</strong>
-            </Text>
-            <Text>
-              Contact: <strong>{contactNo}</strong>
-            </Text>
+          <Box gap="medium" width="90%">
+            <Box width="medium">
+              <Heading level={3} textAlign="start">
+                {name}
+              </Heading>
+              <Heading level={5} textAlign="start" color="gray">
+                {services
+                  .split(",")
+                  .map((service) => {
+                    return service[0].toUpperCase() + service.slice(1);
+                  })
+                  .join(", ")}
+              </Heading>
+            </Box>
+            <Text textAlign="start">" {description} "</Text>
+            <Box direction="row-responsive" gap="xlarge">
+              <Text>
+                Email: <strong>{email}</strong>
+              </Text>
+              <Text>
+                Contact: <strong>{contactNo}</strong>
+              </Text>
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+    </Link>
   );
 };
 
 const DiscoveryList = ({ profiles }) => {
+<<<<<<< Updated upstream
   //console.log(profiles[0]);
+=======
+>>>>>>> Stashed changes
   return (
     <>
       {profiles.length
