80bac3ca8479161cd6f420bfea705fd708cc19dd
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-05-26 09:32:44 +0800

    Fixed Order Completion

--- client/src/components/Profile/ProfileContents/Orders.jsx
+++ client/src/components/Profile/ProfileContents/Orders.jsx
@@ -49,6 +49,7 @@ const Orders = ({ userId, accountType, setProfileComponentIndex }) => {
   const [data, setData] = useState([]);
   const [isValid, setIsValid] = useState(false);
   const { accountState } = useContext(AccountContext);
+  const [childIdx, setChildIdx] = useState(0);
 
   const handleOnClickRow = (e) => {
     const val = e.datum;
@@ -67,13 +68,18 @@ const Orders = ({ userId, accountType, setProfileComponentIndex }) => {
       from = showData.fromUserId;
     }
 
-    validatePayment({
+    const result = validatePayment({
       isValid: 1,
       orderId: showData.id,
       from,
       to,
       accountType: accountState.accountType,
-    });
+    }); // result: {status: <String>}
+    if (result) {
+      if (result.status === "completed")
+        setShowData({ ...showData, status: result.status });
+    }
+    setShow(false);
   };
 
   const handleRate = () => {
@@ -88,14 +94,19 @@ const Orders = ({ userId, accountType, setProfileComponentIndex }) => {
       data[idx].status = "accepted";
       setData([...data]);
     }
+    setShowData({ ...showData, status: "accepted" });
+    setChildIdx(1);
     setShow(false);
   };
   const handleReject = () => {
+    acceptRequest(showData, 0);
     const idx = data.findIndex((val) => val.id === showData.id);
     if (idx) {
       data[idx].status = "rejected";
       setData([...data]);
     }
+    setShowData({ ...showData, status: "rejected" });
+    setChildIdx(1);
     setShow(false);
   };
 
@@ -108,6 +119,16 @@ const Orders = ({ userId, accountType, setProfileComponentIndex }) => {
       setData(orders);
     })();
   }, []);
+
+  useEffect(() => {
+    if (showData) {
+      let idx = 0;
+      if (showData.status === "rejected") idx = 1;
+      else if (showData.status === "accepted") idx = 2;
+
+      setChildIdx(idx);
+    }
+  }, [showData]);
 
   return (
     <Box height="xlarge">
@@ -134,46 +155,24 @@ const Orders = ({ userId, accountType, setProfileComponentIndex }) => {
             </Box>
             {showData.status === "accepted" ? (
               <Text textAlign="center">
-                Click <Text color="red">Validate</Text> button to confirm
+                Click <Text color="red">Complete Order</Text> button to confirm
                 payment
               </Text>
             ) : null}
             <Box direction="row" justify="end" gap="small" align="center">
-              {showData.status === "pending" && accountType === 1 ? (
-                <>
-                  <Box>
-                    <Button
-                      label="Reject"
-                      plain
-                      onClick={handleReject}
-                      margin="1em"
-                    />
-                  </Box>
-                  <Box>
-                    <Button label="Accept" primary onClick={handleAccept} />
-                  </Box>
-                </>
-              ) : (
-                <>
-                  {showData.status === "completed" ? (
-                    <Box>
-                      <Button label="Rate" primary onClick={handleRate} />
-                    </Box>
-                  ) : (
-                    <Box margin="1em">
-                      <Button
-                        label="Rate"
-                        plain
-                        onClick={handleRate}
-                        disabled
-                      />
-                    </Box>
-                  )}
-                  <Box>
-                    <Button label="Validate" primary onClick={handleValidate} />
-                  </Box>
-                </>
-              )}
+              <ShowButtons childIdx={childIdx}>
+                <PendingOrderButtons
+                  handleFns={{ handleAccept, handleReject }}
+                  accountType={accountType}
+                  idx={0}
+                />
+                <RejectedOrder idx={1} />
+                <CompletedOrderButtons
+                  status={showData.status}
+                  handleFns={{ handleRate, handleValidate }}
+                  idx={2}
+                />
+              </ShowButtons>
             </Box>
           </Box>
         </Layer>
@@ -188,12 +187,80 @@ const Orders = ({ userId, accountType, setProfileComponentIndex }) => {
       </Heading>
       <Box overflow="auto">
         <DataTable
+          sort={{ property: "updatedAt", direction: "desc" }}
           columns={columns}
           data={data}
           onClickRow={handleOnClickRow}
         />
       </Box>
     </Box>
+  );
+};
+
+const ShowButtons = ({ children, childIdx }) => {
+  const ChosenComponent = children.find((ch) => {
+    return ch.props.idx === childIdx;
+  });
+
+  return ChosenComponent;
+};
+
+const RejectedOrder = () => {
+  return (
+    <>
+      <Text> Order is rejected</Text>
+    </>
+  );
+};
+
+const PendingOrderButtons = ({ handleFns, accountType }) => {
+  const { handleAccept, handleReject } = handleFns;
+  return (
+    <>
+      {accountType === 1 ? (
+        <>
+          <Box>
+            <Button label="Reject" plain onClick={handleReject} margin="1em" />
+          </Box>
+          <Box>
+            <Button label="Accept" primary onClick={handleAccept} />
+          </Box>
+        </>
+      ) : (
+        <Text>Order is waiting to be accepted...</Text>
+      )}
+    </>
+  );
+};
+
+const CompletedOrderButtons = ({ status, handleFns }) => {
+  const { handleRate, handleValidate } = handleFns;
+
+  return (
+    <>
+      {status === "accepted" && (
+        <>
+          {status === "completed" ? (
+            <Box>
+              <Button label="Rate" primary onClick={handleRate} />
+            </Box>
+          ) : (
+            <>
+              <Box margin="1em">
+                <Button label="Rate" plain onClick={handleRate} disabled />
+              </Box>
+              <Box>
+                <Button
+                  label="Complete Order"
+                  primary
+                  onClick={handleValidate}
+                />
+              </Box>
+            </>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
