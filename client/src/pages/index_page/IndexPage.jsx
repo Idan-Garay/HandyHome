6487d93c27d40233f0b5a14e667ef5b3d3d61148
Idan-Garay <110365012+Idan-Josh@users.noreply.github.com>
2022-10-18 17:15:37 +0800

    new global styles

--- client/src/pages/index_page/IndexPage.jsx
+++ client/src/pages/index_page/IndexPage.jsx
@@ -11,17 +11,9 @@ function IndexPage() {
       <section
         className="border flex-0 flex-col "
       >
-      
-        {/* <div className="searchbar-title  ">
-          <IndexSearchHeadline/>
-        </div>
-        <div className="searchbar  h-24">
-          <IndexSearchInput/>
-        </div> */}
       </section>
       <div
         className=" flex-1 grow flex flex-col sm:flex-row" 
-        // style={{ display: "grid", gridTemplate: "100% / 1fr 3fr" }}
       >
         <IndexSidebar/>
         <IndexContent/>
