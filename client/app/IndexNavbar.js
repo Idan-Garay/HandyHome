a4cfaf45f68fd5a00a256dbe9bd46ca949a64188
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-11-21 21:56:21 +0800

    fix: add flex to navbar

--- client/app/IndexNavbar.js
+++ client/app/IndexNavbar.js
@@ -1,13 +1,12 @@
 import Link from "next/link"
 import Image from"next/image"
 
-
 export default function IndexNavbar() {
-    return <nav className="b">
-        <div className="b">
+    return <nav className="b flex justify-evenly h-20 place-items-center">
+        <div className="b flex-none w-14 min-h-full">
             <Image alt="logo" width="100%" height="100%"/>
         </div>
-        <ul>
+        <ul className="flex space-x-32">
             <li>
                 <Link href="/jobs">Jobs</Link>
             </li>
@@ -19,7 +18,7 @@ export default function IndexNavbar() {
             </li>
         </ul>
 
-        <div className="buttons ">
+        <div className="buttons space-x-5 ">
             <button>Sign up</button>
             <button>Login</button>
         </div>
