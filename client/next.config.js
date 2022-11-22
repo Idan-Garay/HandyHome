01364442b8e43333ef3acd50eb44a7c8f94fc157
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-11-22 21:41:11 +0800

    init

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   experimental: {
-    appDir: true, fontLoaders: [
-      { loader: '@next/font/google', options: { subsets: ['latin'] } },
-    ],
+    appDir: true,
   },
 }
 
