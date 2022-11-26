30c00c67d6eca2650c0ceb8dcd216e34dab5e332
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-11-27 06:21:56 +0800

    fix: add hero background

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -21,7 +21,17 @@ export default function Home() {
         </nav>
       </header>
       <main className='bg-white h-full'>
-        <section className='hero h-screen bg-secondaryColor'>here</section>
+        <section className='hero h-screen bg-secondaryColor bg-[url(/images/hero_bg.svg)] bg-right-bottom bg-cover bg-no-repeat'>here</section>
+        <section className='hero h-screen bg-white'>For Employers</section>
+        <section className='strip h-20 border bg-white flex flex-col'>
+          <h4>Hire talents or get hired for your dream job</h4>
+          <div className="flex space-x-5  ">
+            <button className='button border'>Post a job</button>
+            <p><strong>or</strong></p>
+            <button className='button border'>Hire a client</button>
+          </div>
+        </section>
+        <section className='hero h-screen bg-secondaryColor'>For Freelancers</section>
       </main>
     </>
   )
