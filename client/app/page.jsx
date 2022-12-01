549324535ba81a73f73a2c7bed7cce79845daf53
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2022-12-01 21:35:15 +0800

    fix: add responsiveness to navigation bar

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,4 +1,9 @@
+"use client"
 import Image from 'next/image'
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
+import Navbar from './Navbar';
 // import styles from './page.module.css'
 
 const Logo = () => {
@@ -16,27 +21,32 @@ const AvatarCard = ({ img, title, description }) => {
 }
 
 export default function Home() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='w-full h-full'>
-      <header className='h-20 w-full flex'>
-        <nav className='h-full min-w-full flex flex-row md:flex-row'>
-          <div className="flex flex-0 min-w-fit w-3/12 justify-end">
-            <Logo />
+      <header className='h-20 w-full fixed '>
+        {/* <Navbar />  */}
+        <nav className='h-full min-w-full flex justify-between px-[2%] sm:px-[12%] bg-transparent transition-all z-50 top-0 right-0'>
+          <div className="flex flex-0 min-w-fit justify-end ">
+            <Logo /> 
           </div>
 
-          <ul className="w-full hidden flex-col flex-1 sm:flex sm:flex-row justify-center">
-            <li className=" mx-4 py-7">Jobs</li>
-            <li className=" mx-4 py-7">FAQs</li>
-            <li className=" mx-4 py-7">About</li>
-          </ul>
+          <ul className={`absolute rounded-md top-full right-[2%] flex flex-col h-[29vh] w-[270px] bg-secondaryColor sm:relative sm:top-0 sm:bg-transparent sm:h-full sm:w-full sm:flex-row sm:justify-center `}>
+            <li className="py-2 mx-2 sm:mx-4 sm:py-7">Jobs</li>
+            <li className="py-2 mx-2 sm:mx-4 sm:py-7">FAQs</li>
+            <li className="py-2 mx-2 sm:mx-4 sm:py-7">About</li>
+          </ul> 
 
-          <div className="buttons hidden sm:flex flex-row space-x-3  w-3/12  ">
-            <div className=" py-6">
+          <div className="buttons flex justify-evenly py-6">
               <button className='bg-primaryColor text-secondaryColor rounded-md py-1 px-3 mr-2'>Login</button>
               <button className='bg-secondaryColor text-primaryColor rounded-md py-1 px-3'>Signup</button>
-            </div>
-            <div className="hidden sm:w-2/12"></div>
+              <GiHamburgerMenu size="30" className='m-auto block sm:hidden' color='bg-primaryColor'/>
           </div>
+          {/* <div className="block  sm:hidden h-full w-full"/> */}
+          {/* <div className={`sm:hidden w-40 h-full py-4 cursor-pointer`} onClick={() => setToggleMenu(!toggleMenu)}>
+            {toggleMenu? <AiOutlineClose size="40" className='m-auto' color='bg-primaryColor'/>:<GiHamburgerMenu size="40" className='m-auto ' color='bg-primaryColor'/>}
+          </div> */}
         </nav>
       </header>
       {/* <main className='bg-white h-full'>
