b3af84029c385476c1abca728ff2c3a5ee8785d9
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2023-03-07 21:14:28 +0800

    feat: add hero image

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,16 +6,18 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className="hero">
-        <div className="content">
+    <main className="h-full bg-secondary">
+      <div className="hero border min-w-full flex justify-between min-h-[91.5vh] z-0 relative">
+        <div className="absolute content z-20">
           <h1>HandyHome: Job Marketplace</h1>
           <p>Find to employ or post to get employed</p>
           <div className="empty_space"/>
-          <button className='button '>Post a Job</button>
+          <button className='button min-h-[3rem] min-w-[8rem] rounded bg-primary text-white border hover:bg-black hover:ease-in-out hover:duration-300'>Post a Job</button>
         </div>
-
-        <div className="hero_icon"></div>
+        
+        <div className="hidden md:block absolute z-10 hero_icon h-full min-w-full "> 
+          <Image src="/air_support.png" className=' h-full w-full max-w-full object-cover object-center' height={720} width={1920} alt="hero_icon"/>
+        </div>
       </div>
     </main>
   )
