aca8789c7f02c1991d8463ec254e5ae3f297a748
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2023-03-08 15:30:04 +0800

    fix: make hero section a component

---
+++ client/app/faqs/page.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function FAQS() {
+  return (
+    <main className="h-full bg-secondary">
+      <div className="hero border min-w-full flex justify-between min-h-[91.5vh] z-0 relative">
+        <div className="absolute content z-20 pl-12 md:pl-24 pt-20">
+          <h1 className=' font-semibold text-3xl'>FAQS</h1>
+          <div className="h-3"/>
+          <h3 className='text-xl'>Find to employ or post to get <br/> employed</h3>
+          <div className="h-9"/>
+          <button className='button min-h-[3rem] min-w-[8rem] rounded bg-primary text-white border hover:bg-black hover:ease-in-out hover:duration-300'>Post a Job</button>
+        </div>
+        
+        <div className="hidden md:block absolute z-10 hero_icon h-full min-w-full pt-4"> 
+          <Image src="/air_support.png" className=' h-full w-full max-w-full object-cover object-top' height={720} width={1920} alt="hero_icon"/>
+        </div>
+      </div>
+    </main>
+  )
+}
