031b8a4ef7d508ee562add623ef352500fed1426
Idan Garay <47768819+Idan-Garay@users.noreply.github.com>
2023-03-08 15:56:59 +0800

    fix: adjust gap between screens

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="h-full bg-secondary">
       <Hero/>
-      <div className="min-h-[100vh] max-h-[100vh] w-full border bg-white md:bg-secondary flex flex-col items-center py-12 gap-y-12">
+      <div className="min-h-[100vh] max-h-[100vh] w-full border bg-white md:bg-secondary flex flex-col items-center py-6 md:py-12  gap-y-8 md:gap-y-12">
         <h2 className='text-3xl font-semibold'>For Employers</h2>
         <div className='flex flex-col gap-5 md:flex-row justify-center'>
           <div className="bg-secondary md:bg-white px-5 pt-5 min-h-[5rem] md:min-h-[12rem] h-[40vh] w-[35vh] md:h-[55vh] md:w-[50vh] border-highlight border-2 rounded text-center">
